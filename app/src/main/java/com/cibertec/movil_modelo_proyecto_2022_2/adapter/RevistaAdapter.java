ec11aea2493a42e09ffb623c874e3afd4f99f42e
antoniosoraluz <soraluztorresjoseantonio@gmail.com>
2022-10-22 23:56:33 -0500

    Se agregó funcionalidad al boton filtrar en el menu consultar/revista por Antonio Soraluz

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/adapter/RevistaAdapter.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/adapter/RevistaAdapter.java
@@ -1,12 +1,24 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.cibertec.movil_modelo_proyecto_2022_2.R;
 import com.cibertec.movil_modelo_proyecto_2022_2.entity.Revista;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 public class RevistaAdapter extends ArrayAdapter<Revista>  {
@@ -20,4 +32,45 @@ public class RevistaAdapter extends ArrayAdapter<Revista>  {
         this.lista = lista;
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+
+        View row = inflater.inflate(R.layout.activity_revista_consulta_item, parent, false);
+        Revista objRevista = lista.get(position);
+
+        TextView txtID = row.findViewById(R.id.idConsultaRevItemId);
+        txtID.setText(String.valueOf(objRevista.getIdRevista()));
+
+        TextView txtReviNombre = row.findViewById(R.id.idConsultaRevItemNombre);
+        txtReviNombre.setText(objRevista.getNombre());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String ruta ;
+                    if (objRevista.getIdRevista() == 1){
+                        ruta = "https://i.postimg.cc/WbcDv1Bk/gente.jpg";
+                    }else if (objRevista.getIdRevista() == 2){
+                        ruta = "https://i.postimg.cc/SQMX3Mv4/caretas.jpg";
+                    }else if (objRevista.getIdRevista() == 3){
+                        ruta = "https://i.postimg.cc/0NYsWC9B/etiquetanegra.jpg";
+                    }else{
+                        ruta = "https://i.postimg.cc/SRvF2TGJ/no-disponible.png";
+                    }
+                    URL rutaImagen  = new URL(ruta);
+                    InputStream is = new BufferedInputStream(rutaImagen.openStream());
+                    Bitmap b = BitmapFactory.decodeStream(is);
+                    ImageView vista = row.findViewById(R.id.idConsultaRevImagen);
+                    vista.setImageBitmap(b);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        return row;
+    }
 }
--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/consulta/RevistaConsultaActivity.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/consulta/RevistaConsultaActivity.java
@@ -1,20 +1,71 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.vista.consulta;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.cibertec.movil_modelo_proyecto_2022_2.R;
+import com.cibertec.movil_modelo_proyecto_2022_2.adapter.RevistaAdapter;
+import com.cibertec.movil_modelo_proyecto_2022_2.entity.Revista;
+import com.cibertec.movil_modelo_proyecto_2022_2.service.ServiceRevista;
+import com.cibertec.movil_modelo_proyecto_2022_2.util.ConnectionRest;
 import com.cibertec.movil_modelo_proyecto_2022_2.util.NewAppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class RevistaConsultaActivity extends NewAppCompatActivity {
+
+    ListView lstRevista;
+    ArrayList<Revista> data = new ArrayList<Revista>();
+    RevistaAdapter adaptador;
+
+    Button btnFiltrar;
+
+    ServiceRevista serviceRevista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_revista_consulta);
+
+        btnFiltrar = findViewById(R.id.idConsReviFiltrar);
+
+        lstRevista = findViewById(R.id.idConsReviListView);
+        adaptador = new RevistaAdapter(this, R.layout.activity_revista_consulta_item, data);
+        lstRevista.setAdapter(adaptador);
+
+        serviceRevista = ConnectionRest.getConnection().create(ServiceRevista.class);
+
+        btnFiltrar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View view) {
+                listar();
+            }
+        });
     }
+    public void listar() {
+        Call<List<Revista>> call = serviceRevista.listaRevista();
+        call.enqueue(new Callback<List<Revista>>() {
+            @Override
+            public void onResponse(Call<List<Revista>> call, Response<List<Revista>> response) {
+                List<Revista> lstSalida = response.body();
+                data.clear();
+                data.addAll(lstSalida);
+                adaptador.notifyDataSetChanged();
+            }
 
+            @Override
+            public void onFailure(Call<List<Revista>> call, Throwable t) {
 
-
-
+            }
+        });
+    }
 }
