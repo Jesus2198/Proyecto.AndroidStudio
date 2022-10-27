3d31e315b774052f9b081aaf683fe306da33ca3d
alvaro <alvaro@hotmail.com>
2022-10-26 21:52:11 -0500

    CL2 - Alvaro Guerrero Alvarez - Consulta Libro

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/adapter/LibroAdapter.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/adapter/LibroAdapter.java
@@ -1,12 +1,25 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.cibertec.movil_modelo_proyecto_2022_2.R;
 import com.cibertec.movil_modelo_proyecto_2022_2.entity.Libro;
+
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 public class LibroAdapter extends ArrayAdapter<Libro>  {
@@ -18,6 +31,56 @@ public class LibroAdapter extends ArrayAdapter<Libro>  {
         super(context, resource, lista);
         this.context = context;
         this.lista = lista;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.activity_libro_consulta_item, parent, false);
+
+        Libro objLibro = lista.get(position);
+
+        TextView txtID = row.findViewById(R.id.idLibroItemId);
+        txtID.setText(String.valueOf(objLibro.getIdLibro()));
+
+        TextView  txtTitulo = row.findViewById(R.id.idLibroItemTitulo);
+        txtTitulo.setText(String.valueOf(objLibro.getTitulo()));
+
+        TextView  txtSerie = row.findViewById(R.id.idLibroSerie);
+        txtSerie.setText(String.valueOf(objLibro.getSerie()));
+
+
+        TextView  txtCategoria = row.findViewById(R.id.idConsultaItemCategoria);
+        txtCategoria.setText(String.valueOf(objLibro.getCategoria().getDescripcion()));
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String ruta ;
+                    if (objLibro.getCategoria().getIdCategoria() == 1 ){
+                        ruta = "https://i.postimg.cc/pXKZKRNT/libro1.jpg";
+                    }else if (objLibro.getCategoria().getIdCategoria() == 2){
+                        ruta = "https://i.postimg.cc/bvVHqvn6/libro2.png";
+                    }else if (objLibro.getCategoria().getIdCategoria() == 3){
+                        ruta = "https://i.postimg.cc/4dr6hFBV/libro3.png";
+                    }else if (objLibro.getCategoria().getIdCategoria() == 4){
+                        ruta = "https://i.postimg.cc/brkQ0HBM/libro4.png";
+                    }else{
+                        ruta = "https://i.postimg.cc/DyQr86s4/libro5.jpg";
+                    }
+                    URL rutaImagen  = new URL(ruta);
+                    InputStream is = new BufferedInputStream(rutaImagen.openStream());
+                    Bitmap b = BitmapFactory.decodeStream(is);
+                    ImageView vista = row.findViewById(R.id.idLibroItemImagen);
+                    vista.setImageBitmap(b);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        return row;
     }
 
 }
--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/consulta/LibroConsultaActivity.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/consulta/LibroConsultaActivity.java
@@ -1,12 +1,32 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.vista.consulta;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.cibertec.movil_modelo_proyecto_2022_2.R;
+import com.cibertec.movil_modelo_proyecto_2022_2.adapter.LibroAdapter;
+import com.cibertec.movil_modelo_proyecto_2022_2.entity.Libro;
+import com.cibertec.movil_modelo_proyecto_2022_2.service.ServiceLibro;
+import com.cibertec.movil_modelo_proyecto_2022_2.util.ConnectionRest;
 import com.cibertec.movil_modelo_proyecto_2022_2.util.NewAppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class LibroConsultaActivity extends NewAppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class    LibroConsultaActivity extends NewAppCompatActivity {
+
+    ListView lstLibro;
+    ArrayList<Libro> data = new ArrayList<>();
+    LibroAdapter adapter;
+    Button btnFiltrar;
+
+    ServiceLibro serviceLibro;
 
 
 
@@ -15,9 +35,43 @@ public class LibroConsultaActivity extends NewAppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_libro_consulta);
 
+        btnFiltrar=findViewById(R.id.idConsLibroFiltrar);
+
+        lstLibro=findViewById(R.id.idConsLibroListView);
+        adapter=new LibroAdapter(this,R.layout.activity_libro_consulta_item,data);
+        lstLibro.setAdapter(adapter);
+
+        serviceLibro= ConnectionRest.getConnection().create(ServiceLibro.class);
+
+
+        btnFiltrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lista();
+            }
+        });
+
 
     }
 
+    private void lista(){
+        Call<List<Libro>> call=serviceLibro.listaLibro();
+        call.enqueue(new Callback<List<Libro>>() {
+            @Override
+            public void onResponse(Call<List<Libro>> call, Response<List<Libro>> response) {
+                if(response.isSuccessful()){
+                    List<Libro> lstSalida=response.body();
+                    data.clear();
+                    data.addAll(lstSalida);
+                    adapter.notifyDataSetChanged();}
+            }
+
+            @Override
+            public void onFailure(Call<List<Libro>> call, Throwable t) {
+
+            }
+        });
+    }
 
 
 }
